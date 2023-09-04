b7aec25c00a16aa1de305ef3aa24c103924b5c24
isilva <iury.silva@w3as.com.br>
2023-09-04 15:11:17 -0300

    tests: adiciona teste para busca por cpf

--- main_test.go
+++ main_test.go
@@ -8,12 +8,29 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/guilhermeonrails/api-go-gin/controllers"
+	"github.com/guilhermeonrails/api-go-gin/database"
+	"github.com/guilhermeonrails/api-go-gin/models"
 	"github.com/stretchr/testify/assert"
 )
 
+var ID int
+
 func SetupDasRotasDeTeste() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
 	rotas := gin.Default()
 	return rotas
+}
+
+func CriaAlunoMock() {
+	aluno := models.Aluno{Nome: "Nome do Aluno Teste", CPF: "12345678901",
+		RG: "123456789"}
+	database.DB.Create(&aluno)
+	ID = int(aluno.ID)
+}
+
+func DeletaAlunoMock() {
+	var aluno models.Aluno
+	database.DB.Delete(&aluno, ID)
 }
 
 func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
@@ -22,8 +39,20 @@ func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/gui", nil)
 	resposta := httptest.NewRecorder()
 	r.ServeHTTP(resposta, req)
-	assert.Equal(t, resposta.Code, http.StatusOK, "Deveriam ser iguais")
+	assert.Equal(t, http.StatusOK, resposta.Code, "Deveriam ser iguais")
 	mockDaResposta := `{"API diz:":"E ai gui, tudo beleza?"}`
 	respostaBody, _ := io.ReadAll(resposta.Body)
 	assert.Equal(t, mockDaResposta, string(respostaBody))
+}
+
+func TestBuscaALunoPorCPFHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
+	req, _ := http.NewRequest("GET", "/alunos/cpf/12345678901", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
 }
